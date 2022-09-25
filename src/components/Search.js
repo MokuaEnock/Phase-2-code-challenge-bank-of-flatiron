958c7a8b6f1547d0e228e6c0dc1d75e3a13123b9
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-09-26 01:07:55 +0300

    add serach

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Search() {
+function Search({ input }) {
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder="Search your Recent Transactions"
-        onChange={() => console.log("Searching...")}
+        onChange={(event) => input(event.target.value.toLocaleUpperCase())}
       />
       <i className="circular search link icon"></i>
     </div>
